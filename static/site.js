ee26a0f4cdc0535f5ab9241759a406a30e584560
Nina Boonyasiti <ninaboonyasiti@ksu.edu>
2022-06-15 19:50:44 +0000

    trying to incorporate site.js

--- static/site.js
+++ static/site.js
@@ -1,1 +1,11 @@
-// var img = 
+let map;
+
+function initMap() {
+  const flint = { lat: 39.176576, lng: -96.55997 };
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: flint,
+  });
+}
+
+window.initMap = initMap;
