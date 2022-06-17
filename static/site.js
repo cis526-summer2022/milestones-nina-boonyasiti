cebbedfc63349c876a29d3a0e298fc9a7c2282fb
Nina Boonyasiti <ninaboonyasiti@ksu.edu>
2022-06-17 22:15:54 +0000

    generated unique google static map apis

--- static/site.js
+++ static/site.js
@@ -3,13 +3,19 @@ function displayCard(data) {
   let lat = "";
   let long = "";
   var map = document.createElement("img");
+  let url = "https://maps.googleapis.com/maps/api/staticmap";
+  let mapUrl = "";
 
   for(let i = 0; i < data.length; i++) {
     name = data[i].name;
     lat = data[i].lat;
     long = data[i].lng;
     console.log("Data: ", i, name, lat, long);
+    mapUrl = `url(${url}?center=${lat},${long}&zoom=15&size=200x200&markers=size:mid%7Ccolor:purple%7C${lat},${long}&key=AIzaSyCX8V4KyBtUv7RdOXkeZyGa11wrxlTSVx0
+)`;
+  console.log("mapURL: ", mapUrl);
   }
+
 
 }
 
