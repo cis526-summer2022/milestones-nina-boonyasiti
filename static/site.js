a559e8a1af373cc5868c4def9b7b5142dacdf284
Nina Boonyasiti <ninaboonyasiti@ksu.edu>
2022-06-20 22:15:48 +0000

    Final commit for milestone 2

--- static/site.js
+++ static/site.js
@@ -1,26 +1,29 @@
 function displayCard(data) {
-  let name = "";
-  let lat = "";
-  let long = "";
-  var map = document.createElement("img");
-  let url = "https://maps.googleapis.com/maps/api/staticmap";
-  let mapUrl = "";
-
+  // Milestone 2: Task 2 - converts JSON text into an array of objects
+  // Milestone 2: Task 3 - iterate over the array of JSON objects
   for(let i = 0; i < data.length; i++) {
-    name = data[i].name;
-    lat = data[i].lat;
-    long = data[i].lng;
-    console.log("Data: ", i, name, lat, long);
-    mapUrl = `url(${url}?center=${lat},${long}&zoom=15&size=200x200&markers=size:mid%7Ccolor:purple%7C${lat},${long}&key=AIzaSyCX8V4KyBtUv7RdOXkeZyGa11wrxlTSVx0
-)`;
-  console.log("mapURL: ", mapUrl);
+    const lat = data[i].lat;
+    const long = data[i].lng;
+    const description = document.createTextNode(data[i].name);
+
+    const cardContainer = document.getElementById("card-container");
+    const card = document.createElement("div");
+    const googleMap = document.createElement("img");
+    const text = document.createElement("p");
+    const mapURL = `https://maps.googleapis.com/maps/api/staticmap?censer=${lat},${long}&zoom=15&size=200x200&markers=size:mid%7Ccolor:purple%7C${lat},${long}&key=AIzaSyCX8V4KyBtUv7RdOXkeZyGa11wrxlTSVx0`;
+    
+  // Milestone 2: Task 4 - attatch the DOM elements to the dynamically created cards.
+    text.appendChild(description);
+    card.appendChild(googleMap);
+    card.appendChild(text);
+    cardContainer.appendChild(card);
+    googleMap.src = mapURL;
   }
-
-
 }
 
 function useXHR() {
   const xhr = new XMLHttpRequest();
+  // Milestone 2: Task 1 - Fetch box-locations.json
   const url = './box-locations.json';
   xhr.addEventListener('load', () => {
     displayCard(JSON.parse(xhr.responseText));
