2a7a3b3949928e95546c6b7af21765a6a48cf2d1
Nina Boonyasiti <ninaboonyasiti@ksu.edu>
2022-07-12 08:12:23 +0000

    final commit for milestone 4

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 const path = require('path');
 const express = require('express');
 const serveHomepage = require('./endpoints/serve-homepage');
-const newBox = require('./endpoints/new-box.js');
 const parseBody = require ('./middleware/parse-body.js');
 const createRequest = require('./endpoints/create-request.js');
 const showBox = require('./endpoints/show-box.js');
-const boxDirectory = require('./endpoints/box-directory.js');
-const getCommunityChests = require('./endpoints/get-community-chests.js');
+
 require('../server');
 var bodyParser = require('body-parser');
 
@@ -18,24 +16,8 @@ app.use(express.static('static'));
 
 app.get('/', serveHomepage);
 
-// app.get('/chests', getCommunityChests)
 app.get('/boxes/:id', showBox);
 app.post('/box-locations/:id/requests', createRequest);
-
-/**
- * FOR SUBMITTING
- * Create a new POST endpoint /box-locations/:id/requests
- * Create a new file to hold the function for handling data
- * -- Before you do anything --
- * 		- Create a new table called requests
- * -- Function should: 
- * 		- retrieve post req info
- * 		- create new entry in chest-requests using the box id as primary key
- * -- Updating Page (/boxes/:id, show-box(.html and .js))
- * 		- Do db.prepare to get all chest-requests from chest-request
- * 		- in your template you must update html to include the list of chest-request ([])
- * 		- Update your show-box.html.ejs to display all the chest request on the page
- */
 
 
 module.exports = app;
--- src/endpoints/get-community-chests.js
+++
@@ -1,9 +0,0 @@
-const Database = require('better-sqlite3');
-const db = require('../database');
-
-function getCommunityChests(req, res) {
-  const chests = db.prepare('SELECT * FROM boxes;').all();
-  res.send(chests);
-}
-
-module.exports = getCommunityChests;
