f7a37a3d3a010b3d98a4e9c5f4f2c0fc078df527
Nina Boonyasiti <ninaboonyasiti@ksu.edu>
2022-07-26 19:09:15 +0000

    fixed non auth user visitation bug

--- src/middleware/authors-only.js
+++ src/middleware/authors-only.js
@@ -4,7 +4,7 @@ function authorsOnly(req, res, next) {
   var session = req.session;
   console.log("REQ. SESSION: ", req.session);
   if(req.session === false) next();
-  if(req.session.user.roles === 1) next();
+  if(req.session.user.roles === 1 || req.session.user.roles === 0) next();
   else serveError(req, res, 403, `User with role ${req.session.user.roles} attempted to use an author-only route`);
 }
 
